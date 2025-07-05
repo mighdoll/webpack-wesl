115cf9b1546765ac514422bce727da3617951dcd
lee mighdoll <lee@underneath.ca>
2025-07-05 15:38:12 -0700

    simplify script

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
 import wgsl from "../shaders/app.wesl?static";
 
-function component() {
-  const element = document.getElementById('code');
-
-  element.innerHTML = wgsl;
-
-  return element;
-}
-
-document.body.appendChild(component());
+const element = document.getElementById("code");
+element.innerHTML = wgsl;
